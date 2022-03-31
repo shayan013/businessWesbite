3f7097bc66f7e6b8511faa880d399022f0646152
shayan013 <shayanakhtar013@gmail.com>
2022-03-31 18:42:25 +0500

    Updates

---
+++ precache-manifest.56363b70433842e236bdb524dd4f0a24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9496ec3fd950fd4b49b5878cfefcf9",
+    "url": "/portfoliowebsite/index.html"
+  },
+  {
+    "revision": "378dce055c7d08e07a8e",
+    "url": "/portfoliowebsite/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "057dd9e735daf3dde700",
+    "url": "/portfoliowebsite/static/css/main.e86f7aed.chunk.css"
+  },
+  {
+    "revision": "378dce055c7d08e07a8e",
+    "url": "/portfoliowebsite/static/js/2.187b711c.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/portfoliowebsite/static/js/2.187b711c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057dd9e735daf3dde700",
+    "url": "/portfoliowebsite/static/js/main.8e3f64bb.chunk.js"
+  },
+  {
+    "revision": "41febcbb51eb98fa0da6",
+    "url": "/portfoliowebsite/static/js/runtime-main.8c1076ad.js"
+  }
+]);
